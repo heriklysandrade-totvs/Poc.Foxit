bf0e3d8e82b2e57ec0e0d23cf824e3b47ba51b4a
Heriklys Alvaro De Almeida Andrade <heriklys.andrade@totvs.com.br>
2024-01-18 14:58:25 -0300

    Criação de controller para teste.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Poc.Foxit.Api.Responses;
+
+namespace Poc.Foxit.Api.Controllers
+{
+    [Route("v1/test")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            Console.WriteLine("**LOCAL CONSOLE** Initializing Test Controller");
+        }
+
+        [HttpGet("hello-world")]
+        public IActionResult HelloWorld()
+        {
+            return Ok(new OkResponse("Hello world"));
+        }
+    }
+}
